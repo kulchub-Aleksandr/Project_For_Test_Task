b4b28b7badbae781409549d2a3d20d6bd76ce28a
kulchub-Aleksandr <kulchub@gmail.com>
2026-05-04 12:07:23 +0500

    add tag

--- src/test/java/tests/BankTest.java
+++ src/test/java/tests/BankTest.java
@@ -1,11 +1,13 @@
 package tests;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class BankTest extends TestBase {
 
     @DisplayName("Вход в личный кабинет банка")
+    @Tag("UI")
     @Test
     public void loginInBank() {
         mainPage.openPage()
@@ -15,6 +17,7 @@ public class BankTest extends TestBase {
     }
 
     @DisplayName("Вход в личный кабинет банка, используя неполный номер телефона")
+    @Tag("UI")
     @Test
     public void loginInBankUseIncompletePhoneNumber() {
         mainPage.openPage()
@@ -26,6 +29,7 @@ public class BankTest extends TestBase {
     }
 
     @DisplayName("Выбор страницы кредита в банке")
+    @Tag("UI")
     @Test
     public void chooseCredit() {
         mainPage.openPage()
@@ -35,6 +39,7 @@ public class BankTest extends TestBase {
     }
 
     @DisplayName("Выбор страховки ОСАГО в банке")
+    @Tag("UI")
     @Test
     public void coverageInBank() {
         mainPage.openPage()
@@ -45,6 +50,7 @@ public class BankTest extends TestBase {
     }
 
     @DisplayName("Выбор страницы Топливо на веб-сайте банка")
+    @Tag("UI")
     @Test
     public void fuelInBank() {
 
